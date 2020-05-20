ca11787c595e8595b9782d310128aace461ffb97
atomink <atomink@gmail.com>
2020-05-20 07:58:35 +0300

    ++ added post struct and saveposts(inmemory)

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"BeardBar_on_GO/models"
 	"fmt"
 	"html/template"
 	"net/http"
 )
+
+var posts map[string]*models.Post
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
@@ -12,13 +15,38 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, err.Error())
 	}
 
+	fmt.Println(posts)
+
 	t.ExecuteTemplate(w, "index", nil)
+}
+
+func writeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "write", nil)
+}
+
+func savePostHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	title := r.FormValue("title")
+	content := r.FormValue("content")
+
+	post := models.NewPost(id, title, content)
+	posts[post.Id] = post
+
+	http.Redirect(w, r, "/", 302)
 }
 
 func main() {
 	fmt.Println("Listening on port :8080")
+	posts = make(map[string]*models.Post, 0)
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/write", writeHandler)
+	http.HandleFunc("/SavePost", savePostHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
